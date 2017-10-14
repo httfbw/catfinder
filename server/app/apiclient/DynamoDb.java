5cdb2d12eacb62bda72035d75807f94a7c419f8b
Stefan Schlott <stefan@ploing.de>
2017-10-14 17:45:30 +0200

    Web service: Uploads geopos

--- server/app/apiclient/DynamoDb.java
+++ server/app/apiclient/DynamoDb.java
@@ -1,4 +1,4 @@
-package apiclients;
+package apiclient;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -8,29 +8,34 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.google.common.collect.ImmutableMap;
-import org.junit.Test;
-import play.test.WithApplication;
+import com.typesafe.config.Config;
 
+import javax.inject.Inject;
 import java.util.Map;
 
 
-public class AwsTest extends WithApplication {
-    @Test
-    public void testDataUpload() {
-        final String tableName = provideApplication().config().getString("aws.tablename");
-        final String accessKey = provideApplication().config().getString("aws.accessKey");
-        final String secretKey = provideApplication().config().getString("aws.secretKey");
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+public class DynamoDb {
+    String tableName;
+    AWSCredentials credentials;
+
+    @Inject
+    public DynamoDb(Config cfg) {
+        tableName = cfg.getString("aws.tablename");
+        String accessKey = cfg.getString("aws.accessKey");
+        String secretKey = cfg.getString("aws.secretKey");
+        credentials = new BasicAWSCredentials(accessKey, secretKey);
+    }
+
+    public void putData(String name, double lon, double lat) {
         AmazonDynamoDB db = AmazonDynamoDBClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(Regions.EU_WEST_1)
                 .build();
         Map<String, AttributeValue> item = ImmutableMap.<String, AttributeValue>builder()
-                .put("name", new AttributeValue("Mietze"))
-                .put("lon", new AttributeValue("9.236"))
-                .put("lat", new AttributeValue("48.7770"))
+                .put("name", new AttributeValue(name))
+                .put("lon", new AttributeValue(Double.toString(lon)))
+                .put("lat", new AttributeValue(Double.toString(lat)))
                 .build();
         db.putItem(tableName, item);
     }
 }
-
